d12797427a17beb49ca62f42908c90d7662889d0
yaaan7 <wangpajeon@gmail.com>
2026-08-12 04:30:55 +0900

    feat:username 조회

--- src/main/java/com/lounge/domain/user/service/UserService.java
+++ src/main/java/com/lounge/domain/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.lounge.domain.user.service;
 
+import com.lounge.domain.user.dto.response.UserMeResponse;
+import com.lounge.global.api.code.GeneralErrorCode;
+import com.lounge.global.exception.GeneralException;
 import com.lounge.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserMeResponse getMe(Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> UserMeResponse.of(user.getUsername()))
+                .orElseThrow(() -> GeneralException.of(GeneralErrorCode.USER_NOT_FOUND));
+    }
 }
